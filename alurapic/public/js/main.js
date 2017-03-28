f779294c11cab89b71f52735481f71401623c5ba
cust6781 <cust6781@caelumBSB02-08>
2017-03-28 17:53:47 -0300

    Aula2 - Cap5 - Rotas

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,1 +1,12 @@
-angular.module('alurapic', ['bootstrapDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['bootstrapDiretivas', 'ngAnimate', 'ngRoute'])
+  .config(function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+    $routeProvider.when('/fotos', {
+      templateUrl: 'partials/principal.html',
+      controller: 'FotosController'
+    }).when('/fotos/new', {
+      templateUrl: 'partials/foto.html'
+    // });
+    // $routeProvider.otherwise({redirectTo: '/fotos'});
+    }).otherwise({redirectTo: '/fotos'});
+  });
