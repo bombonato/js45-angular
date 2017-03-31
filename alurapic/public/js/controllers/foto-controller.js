5505c1ff56fba28e931deb7cb3b1e64bef283440
cust6781 <cust6781@caelumBSB02-08>
2017-03-31 16:41:55 -0300

    Aula5-Cap10-Servicos

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,18 +1,10 @@
 angular
 .module('alurapic')
-.controller('FotoController', function($scope, recursoFoto, $routeParams) {
+.controller('FotoController', function($scope, recursoFoto, $routeParams, cadastroDeFotos) {
   $scope.foto = {};
   $scope.mensagem = '';
 
   if($routeParams.fotoId){
-    // $http.get('/v1/fotos/' + $routeParams.fotoId)
-    //       .success(function(foto) {
-    //         $scope.foto = foto;
-    //       })
-    //       .error(function(erro) {
-    //         console.log(erro);
-    //         $scope.mensagem = "Nao foi possivel obter a foto";
-    //       })
     recursoFoto.get({ fotoId: $routeParams.fotoId}, function(foto) {
             $scope.foto = foto;
           }, function(erro) {
@@ -27,50 +19,42 @@ angular
     // Somente se todos os campos do formulario forem validados
     if ($scope.formulario.$valid) {
 
-      if($routeParams.fotoId) {
-
-        // $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto )
-        // .success(function() {
-        //   console.log("Foto alterada");
-        //   $scope.mensagem = 'Foto alterada com sucesso';
-        // })
-        // .error(function() {
-        //   console.log("Foto NÃO alterada");
-        //   $scope.mensagem = 'Erro: foto NÃO alterar';
-        // });
-        recursoFoto.update( {fotoId: $scope.foto._id},
-          $scope.foto, function() {
-          console.log("Foto alterada");
-          $scope.mensagem = 'Foto alterada com sucesso';
-        }, function(erro) {
-          console.log("Foto NÃO alterada");
-          $scope.mensagem = 'Erro: foto NÃO alterada';
-        });
-
-      } else {
-          // $http.post('/v1/fotos', $scope.foto)
-          // .success(function() {
-          //   console.log("Foto adicionada");
-          //   $scope.mensagem = 'Foto cadastrada com sucesso';
-          //   $scope.foto = {};
-          //   $scope.formulario.$setUntouched();
-          //   $scope.formulario.$setPristine();
-          // })
-          // .error(function() {
-          //   console.log("Foto NÃO cadastrada");
-          //   $scope.mensagem = 'Erro: foto NÃO cadastrada';
-          // })
-          recursoFoto.save($scope.foto, function() {
-            console.log("Foto adicionada");
-            $scope.mensagem = 'Foto cadastrada com sucesso';
+      // if($routeParams.fotoId) {
+      //
+      //   recursoFoto.update( {fotoId: $scope.foto._id},
+      //     $scope.foto, function() {
+      //     console.log("Foto alterada");
+      //     $scope.mensagem = 'Foto alterada com sucesso';
+      //   }, function(erro) {
+      //     console.log("Foto NÃO alterada");
+      //     $scope.mensagem = 'Erro: foto NÃO alterada';
+      //   });
+      //
+      // } else {
+      //
+      //     recursoFoto.save($scope.foto, function() {
+      //       console.log("Foto adicionada");
+      //       $scope.mensagem = 'Foto cadastrada com sucesso';
+      //       $scope.foto = {};
+      //       $scope.formulario.$setUntouched();
+      //       $scope.formulario.$setPristine();
+      //     }, function(erro) {
+      //       console.log(erro);
+      //       $scope.mensagem = 'Erro: foto NÃO cadastrada';
+      //     })
+      //   }
+      cadastroDeFotos.cadastrar($scope.foto)
+        .then( function(dados) {
+          $scope.mensagem = dados.mensagem;
+          if(dados.inclusao) {
             $scope.foto = {};
             $scope.formulario.$setUntouched();
             $scope.formulario.$setPristine();
-          }, function(erro) {
-            console.log(erro);
-            $scope.mensagem = 'Erro: foto NÃO cadastrada';
-          })
-        }
+          }
+        })
+        .catch( function(erro) {
+          $scope.mensagem = erro.mensagem;
+        })
     }
   };
 });
--- alurapic/public/js/services/meus-servicos.js
+++ alurapic/public/js/services/meus-servicos.js
@@ -5,4 +5,58 @@ angular.module('meusServicos', ['ngResource'])
               method: 'PUT'
             }
           });
+        })
+        .service('cadastroDeFotos', function (recursoFoto, $q) {
+          this.listar = function() {
+            return $q(function (resolve, reject) {
+                recursoFoto.query(function(fotos){
+                    resolve(fotos);
+                }, function(erro) {
+                    console.log(erro);
+                    reject({
+                      mensagem: "Não foi possivel listar"
+                    });
+                });
+            });
+          }
+
+          this.cadastrar = function(foto) {
+            return $q(function(resolve, reject){
+
+                // Codigo foi transportado de foto-controller
+                if(foto._id) {
+
+                  recursoFoto.update( {fotoId: foto._id}, foto, function() {
+                      console.log("Foto alterada");
+                      resolve({
+                        mensagem : 'Foto ' + foto.titulo + ' alterada com sucesso',
+                        inclusao: false
+                      });
+                  }, function(erro) {
+                    console.log("Foto NÃO alterada: " + erro);
+                    reject({
+                      mensagem : 'Erro: foto ' + foto.titulo + ' NÃO cadastrada'
+                    });
+                  });
+
+                } else {
+
+                    recursoFoto.save(foto, function() {
+                      console.log("Foto adicionada");
+                      resolve({
+                        mensagem : 'Foto ' + foto.titulo + ' cadastrada com sucesso',
+                        inclusao: true
+                        // foto : {};
+                        // formulario.$setUntouched();
+                        // formulario.$setPristine();
+                      });
+                    }, function(erro) {
+                      console.log(erro);
+                      reject({
+                        mensagem : 'Erro: foto ' + foto.titulo + ' NÃO cadastrada'
+                      });
+                    })
+                  }
+            })
+          }
         });
